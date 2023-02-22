9552e3d3bc2f9e86ec8c8fe64e6df73cb2c38271
JustinPratama <jcwpratama@gmail.com>
2023-02-22 17:21:03 -0500

    third commit

--- Code/nc4_2_22.py
+++ Code/nc4_2_22.py
@@ -1,5 +1,5 @@
+#Import data
 import netCDF4 as nc
-
 #Copy the path from the file 
 fn = '/Users/justinpratama/Desktop/Groundwater_Research/Data/GRACEDADM_CLSM0125US_7D.A20230220.020.nc4'
 #Obtain the dataset
